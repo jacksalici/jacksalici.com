7a92fd406282408a0cf5975758f145b98a676934
Giacomo Salici <giambo2000@gmail.com>
2023-03-09 01:12:20 +0100

    intersection observer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 /*
-*
-*   DOTS PATTERN
-*
-*/
+ *
+ *   DOTS PATTERN
+ *
+ */
 
 var svg = document.getElementById("svg");
 var dotMatrix = document.createElementNS(
@@ -21,13 +21,11 @@ var dotRandomMax = 0;
 var mouseMoving = false;
 var mouse = {};
 
-
 var returningSpeed = 1;
 mouse.distances = [];
 mouse.power = 80;
 
 function init() {
-
   screenW = window.innerWidth;
   screenH = window.innerHeight;
   dotColumns = Math.round(screenW / dotStepX);
@@ -43,8 +41,16 @@ function init() {
     dots[i] = {};
     dots[i].distances = [];
     dots[i].el = dotMatrix.cloneNode(false);
-    dots[i].X = k * dotStepX - dotRandomMax /2 + Math.random()*dotRandomMax  + dotStepX / 2;
-    dots[i].Y = j * dotStepY - dotRandomMax /2 + Math.random()*dotRandomMax + dotStepX / 2;
+    dots[i].X =
+      k * dotStepX -
+      dotRandomMax / 2 +
+      Math.random() * dotRandomMax +
+      dotStepX / 2;
+    dots[i].Y =
+      j * dotStepY -
+      dotRandomMax / 2 +
+      Math.random() * dotRandomMax +
+      dotStepX / 2;
 
     dots[i].r = 1;
     dots[i].X0 = dots[i].X;
@@ -54,7 +60,7 @@ function init() {
     dots[i].el.setAttribute("cx", dots[i].X);
     dots[i].el.setAttribute("cy", dots[i].Y);
     dots[i].el.setAttribute("r", dots[i].r);
-    dots[i].el.setAttribute("opacity", dots[i].opacity)
+    dots[i].el.setAttribute("opacity", dots[i].opacity);
     svg.appendChild(dots[i].el);
   }
 }
@@ -66,25 +72,19 @@ function getDistance(obj1, obj2) {
 }
 
 function update() {
-    for (i = 0; i < dotColumns * dotRows; i++) {
-      if (getDistance(dots[i], mouse)<mouse.power/2){
-        dots[i].el.setAttribute("opacity", 0.7)
-        dots[i].el.setAttribute("r", 1.5)
-
-      }else if (getDistance(dots[i], mouse)<mouse.power){
-        dots[i].el.setAttribute("opacity", 0.5)
-        dots[i].el.setAttribute("r", 1.25)
-
-      } else {
-        dots[i].el.setAttribute("opacity", 0.3)
-        dots[i].el.setAttribute("r", 1)
-
-      }
+  for (i = 0; i < dotColumns * dotRows; i++) {
+    if (getDistance(dots[i], mouse) < mouse.power / 2) {
+      dots[i].el.setAttribute("opacity", 0.7);
+      dots[i].el.setAttribute("r", 1.5);
+    } else if (getDistance(dots[i], mouse) < mouse.power) {
+      dots[i].el.setAttribute("opacity", 0.5);
+      dots[i].el.setAttribute("r", 1.25);
+    } else {
+      dots[i].el.setAttribute("opacity", 0.3);
+      dots[i].el.setAttribute("r", 1);
     }
-
+  }
 }
-
-
 
 function destroy() {
   while (svg.firstChild) {
@@ -100,71 +100,106 @@ window.addEventListener("resize", function () {
   init();
 });
 
-
 window.addEventListener("mousemove", function (e) {
   mouseMoving = true;
   mouse.X = e.clientX;
   mouse.Y = e.clientY;
-  update()
+  update();
 });
 
 window.addEventListener("touchmove", function (e) {
   mouseMoving = true;
   mouse.X = e.changedTouches.item(0).clientX;
   mouse.Y = e.changedTouches.item(0).clientY;
-  update()
+  update();
 });
 
 /*
-*
-*   HEADER BAR AND NAV MENU
-*
-*/
+ *
+ *   HEADER BAR AND NAV MENU
+ *
+ */
 
 function menuOnClick() {
   document.getElementById("hamburger").classList.toggle("change");
   document.getElementById("menu-closer").classList.toggle("change");
   document.getElementById("nav").classList.toggle("hide-nav");
-  
-  
-  }
-  
-  function menuCloseOnClick() {
+}
+
+function menuCloseOnClick() {
   document.getElementById("hamburger").classList.toggle("change");
   document.getElementById("nav").classList.toggle("hide-nav");
   document.getElementById("menu-closer").classList.toggle("change");
-  }
+}
 
+if (document.getElementById("hero-text") !== null) {
+  const circleType = new CircleType(
+    document.getElementById("hero-text"),
+    (string) => (string + " ").split("")
+  );
 
-  const circleType = new CircleType(document.getElementById('hero-text'), (string) => (string + ' ').split('') );
-
-
-  const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
-  document.addEventListener("scroll", function() {
+  const vh = Math.max(
+    document.documentElement.clientHeight || 0,
+    window.innerHeight || 0
+  );
+  document.addEventListener("scroll", function () {
     var offset = window.scrollY;
-  
-    document.getElementById('hero-text').style.transform = "rotate(" + offset * .3 + "deg)"
-    document.getElementById('hero-text').style.top = (offset * .3) + (.15 * vh) + "px"
-    
-    
-    
-  })
+
+    document.getElementById("hero-text").style.transform =
+      "rotate(" + offset * 0.3 + "deg)";
+    document.getElementById("hero-text").style.top =
+      offset * 0.3 + 0.15 * vh + "px";
+  });
+}
 /*
-*
-*   TOC
-*
-*/
+ *
+ *   TOC
+ *
+ */
 var collapsibleToc = document.getElementsByClassName("single__toc-button");
 var i;
 
 for (i = 0; i < collapsibleToc.length; i++) {
-  collapsibleToc[i].addEventListener("click", function() {
+  collapsibleToc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = document.getElementsByClassName("single__toc-content")[0];
-    if (content.style.maxHeight){
+    if (content.style.maxHeight) {
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
     }
   });
 }
+
+/*
+ *
+ *   OBSERVER
+ *
+ */
+
+const observer = new IntersectionObserver((entries) => {
+  entries.map((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    } else {
+      entry.target.classList.remove("visible");
+    }
+
+    if (entry.boundingClientRect.top < 0) {
+      entry.target.classList.add("hidden-top");
+    } else {
+      entry.target.classList.remove("hidden-top");
+    }
+
+    if (entry.boundingClientRect.top > 0) {
+      entry.target.classList.add("hidden-bottom");
+    } else {
+      entry.target.classList.remove("hidden-bottom");
+    }
+  });
+});
+
+const observerList = document.querySelectorAll(".animated");
+observerList.forEach((el) => {
+  observer.observe(el);
+});
--- content/digital-craftsman.md
+++
@@ -1,5 +0,0 @@
----
-title: "Digital Craftsman"
-hideMeta: True
----
-
