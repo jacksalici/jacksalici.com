393408a37d539037eab505834b6800bbc24643c7
Giacomo Salici <giambo2000@gmail.com>
2023-07-10 00:32:00 +0200

    daisy init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,44 @@
+const disabledCss = {
+	'code::before': false,
+	'code::after': false,
+	'blockquote p:first-of-type::before': false,
+	'blockquote p:last-of-type::after': false,
+	pre: false,
+	code: false,
+	'pre code': false,
+	'code::before': false,
+	'code::after': false,
+}
+
 module.exports = {
   content: ['./layouts/**/*.html', './content/**/*.md'],
   theme: {
-    extend: {},
+    extend: {
+  		typography: {
+				DEFAULT: { css: disabledCss },
+				sm: { css: disabledCss },
+				lg: { css: disabledCss },
+				xl: { css: disabledCss },
+				'2xl': { css: disabledCss },
+			},
+    },
+    colors:{
+      primary: '#686bed'
+    },
+    fontFamily:{
+      'sans': ['Open Sans', 'sans-serif'],
+      'serif': ['Newsreader', 'serif'],
+      'mono': ['Ubuntu Mono', 'monospace'],
+    }
+   
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+  ],
+  daisyui:{
+    logs: false,
+    themes: ['lofi'],
+    
+  }
 }
